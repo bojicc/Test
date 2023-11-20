cd502c212f1598966527907c2fbb83d9c9bb1ed5
Dejan Bojic <dejan.bojic.001@gmail.com>
2023-11-20 19:55:35 +0100

    Add new Model Brand

---
+++ WebApplication1/WebApplication1/Models/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Brand
+    {
+        public int BrandID { get; set; }
+        public string BrandName { get; set; }
+    }
+}
