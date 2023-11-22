e7705fd2b4ae22a4887bf0eb2c8d57ddcdccd5fc
Dejan Bojic <dejan.bojic.001@gmail.com>
2023-11-22 13:12:45 +0100

    no message

--- WebApplication1/WebApplication1/Models/Brand.cs
+++ WebApplication1/WebApplication1/Models/Brand.cs
@@ -8,6 +8,6 @@ namespace WebApplication1.Models
     public class Brand
     {
         public int BrandID { get; set; }
-        public string BrandName { get; set; }
+        public string Text { get; set; }
     }
 }
